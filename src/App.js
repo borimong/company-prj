07e7c7f77ec923e6c9aabfbdc84bb06478bf04ff
김현수 <gimhyeonsu@gimhyeonsuui-MacBookPro.local>
2022-05-28 01:10:52 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "./Button"; 
+import styles from "./App.module.css";
+import {useState, useEffect} from "react";
+
+function Hello () {
+
+  useEffect(()=> {
+    console.log("hi : )");
+    return () =>
+      console.log("bye :(");
+  }, []);
+
+
+  useEffect(function() {
+    console.log("hi : )");
+    return function () {
+      console.log("bye :(");
+    };
+  }, []);
+
+ 
+
+}
 
 function App() {
+
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing (prev => !prev);
+  return <div>
+    {
+      showing ? <Hello/> : null 
+    }
+    <button onClick = {onClick} >{showing ? "Hide" : "Show"}</button>
+  </div>
+
+  /*const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+
+  const onClick = () => setValue((prev) => prev+1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+
+  
+   
+  useEffect(() => {console.log("i run only once");}, []);
+
+  useEffect(() => {console.log("i run when keyword changes.");}, [keyword]);
+
+  useEffect(() => {console.log("i run when counter changed");}, [counter]);
+
+  useEffect(() => {console.log("i run when counter or keyword changed");}, [counter, keyword]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <input value = {keyword} onChange = {onChange} type="text" placeholder = "Search here"/>
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick = {onClick}>Click me!</button>
+      
+
+      
+    </div> 
+    );
+    */
 }
 
 export default App;
