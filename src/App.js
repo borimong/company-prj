99941eec5fa699b4a08e4650343e60f3bc031c04
김현수 <gimhyeonsu@gimhyeonsuui-MacBookPro.local>
2022-05-28 01:16:45 +0900

    freshman first commit

--- src/App.js
+++ src/App.js
@@ -31,6 +31,7 @@ function App() {
       showing ? <Hello/> : null 
     }
     <button onClick = {onClick} >{showing ? "Hide" : "Show"}</button>
+    <h2>Wow</h2>
   </div>
 
   /*const [counter, setValue] = useState(0);
